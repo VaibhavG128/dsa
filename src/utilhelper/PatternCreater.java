b0a4b1a6052c090f82445649ee0e48527ae0d400
VAIBHAV <VAIBHAV@DESKTOP-2AN8JNJ>
2023-02-19 21:37:26 +0530

    Complete all patterns

--- src/utilhelper/PatternCreater.java
+++ src/utilhelper/PatternCreater.java
@@ -24,7 +24,7 @@ public class PatternCreater {
 		 	*****
 		 	*****
 		 */
-		for (int i = 1; i <= n; i++) {
+		for(int i = 1; i <= n; i++) {
 			for (int j = 1; j <= n; j++) {
 				System.out.print("*");
 			}
